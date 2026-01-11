18c7d669e94c06718558e895d23901bb3dd572e4
praful-koli <prafulkoli852@gmail.com>
2026-01-11 21:28:52 +0530

    update code

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className=' relative w-full sm:h-screen  bg-[#d22715] text-white'>
         <Nav />
-        <div className="flex flex-col  items-center mt-10 py-8 sm:px-20 sm:mt-0 sm:py-0  sm:flex-row sm:gap-30 ">
+        <div className="flex flex-col justify-center  items-center mt-10 py-8 sm:px-20 sm:mt-0 sm:py-0  sm:flex-row sm:gap-30 ">
 
            <div className="textBox flex flex-col px-8 items-start gap-5  sm:w-90">
             <h1 className='text-lg sm:text-2xl '>01__________</h1>
@@ -14,13 +14,13 @@ function Hero() {
              <button className='active:scale-97 outline-none border-2 px-4 py-1.5 rounded-md text-md font-semibold'>Shop Now</button>
            </div>
    
-         <div className=" ">      
-           <div className="">
-            <img src={image} alt="" />
+           
+           <div className="w-full flex text-center justify-center">
+            <img width={"60%"} src={image} alt="image" />
            </div>
-            <h1 className='text-6xl sm: text-9xl'>BERRIES</h1>
+            <h1 className='text-6xl sm:text-9xl'>BERRIES</h1>
          </div>
-        </div>
+    
     
     </div>
   )
